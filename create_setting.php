15c8bcc42cd2a422e6bb804e95670a42bbd4335c
George Serradinho <serradinho@users.noreply.github.com>
2013-10-08 01:13:04 +0200

    Create Setting function
    Create settings field in WordPress.

---
+++ create_setting.php
@@ -0,0 +1,37 @@
+<?php 
+
+	 * Create settings field
+	 *
+	 * @since 1.0
+	 */
+	public function create_setting( $args = array() ) {
+		
+		$defaults = array(
+			'id'      => 'default_field',
+			'title'   => __( 'Default Field' ),
+			'desc'    => __( 'This is a default description.' ),
+			'std'     => '',
+			'type'    => 'text',
+			'section' => 'general',
+			'choices' => array(),
+			'class'   => ''
+		);
+			
+		extract( wp_parse_args( $args, $defaults ) );
+		
+		$field_args = array(
+			'type'      => $type,
+			'id'        => $id,
+			'desc'      => $desc,
+			'std'       => $std,
+			'choices'   => $choices,
+			'label_for' => $id,
+			'class'     => $class
+		);
+		
+		if ( $type == 'checkbox' )
+			$this->checkboxes[] = $id;
+		
+		add_settings_field( $id, $title, array( $this, 'display_setting' ), 'mytheme-options', $section, $field_args );
+	}
+?>
